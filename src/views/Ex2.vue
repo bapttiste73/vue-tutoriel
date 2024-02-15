7e8684f82347587b5930c03eea5e123a1356423e
bapttiste <bama73000@gmail.com>
2024-02-15 22:18:57 +0100

    init

--- src/views/Ex2.vue
+++ src/views/Ex2.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="title"><h1>Les observateurs</h1></div>
+    <div class="title"><h1>Le binding et liaison dynamique</h1></div>
     <div class="ex">
       <h1>Passez moi en rouge</h1> <!-- ajoutez la liaison de la classe dynamique ici -->
     </div>
